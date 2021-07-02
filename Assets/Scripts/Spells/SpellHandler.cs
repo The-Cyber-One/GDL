fbd99b51e6beaa437d0c33d2ed5e05818e36e16d
nielsduif <niels-duivenvoorden@hotmail.com>
2021-07-02 18:52:09 +0200

    spell resize before dinner

--- Assets/Scripts/Spells/SpellHandler.cs
+++ Assets/Scripts/Spells/SpellHandler.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class SpellHandler : MonoBehaviour
 {
+    public Spell activeSpell;
     public List<Spell> spells;
     public ParticleSystem shootPS;
     public Transform playerEyes;
-    public float range = 50f;
-    public float fireRate = 1, nextFire;
+    public string playerTag = "Player";
+    public LayerMask ObjectLayerMask;
 
     Camera FPS;
 
@@ -16,19 +17,51 @@ public class SpellHandler : MonoBehaviour
     void Start()
     {
         FPS = playerEyes.GetComponent<Camera>();
+
+        foreach (Spell spell in spells)
+        {
+            spell.gameObject.SetActive(false);
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 rayOrigin = FPS.ViewportToScreenPoint(new Vector3(.5f, .5f, .0f));
-        Debug.DrawRay(FPS.transform.position, FPS.transform.forward * range, Color.green);
 
         RaycastHit hit;
-        if (Physics.Raycast(playerEyes.transform.position, FPS.transform.forward, out hit, range))
+        if (Physics.Raycast(playerEyes.transform.position, FPS.transform.forward, out hit, Mathf.Infinity, ObjectLayerMask))
         {
-            Debug.Log(hit.transform.name);
             Debug.DrawLine(playerEyes.transform.position, hit.point, Color.blue);
+
+            if (hit.transform.tag == playerTag)
+            {
+                activeSpell = spells[0];
+                (spells[0] as SpellResize).EnableMe(hit);
+                MakeActive();
+            }
+        }
+        else
+        {
+            activeSpell = null;
+            MakeActive();
+        }
+    }
+
+    void MakeActive()
+    {
+        foreach (Spell spell in spells)
+        {
+            if (activeSpell == spell)
+            {
+                spell.gameObject.SetActive(true);
+            }
+            else
+            {
+                spell.gameObject.SetActive(false);
+            }
         }
     }
 }
--- Assets/Scripts/Spells/SpellResize.cs
+++ Assets/Scripts/Spells/SpellResize.cs
@@ -4,16 +4,57 @@ using UnityEngine;
 
 public class SpellResize : Spell
 {
-    
+    Transform otherPlayer;
+    Transform root;
+    float initialSize, initialDistance, finalDistance;
+    public LayerMask NoObjectLayerMask;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonUp(0) && otherPlayer != null)
+        {
+            otherPlayer = null;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (otherPlayer == null)
+        {
+            return;
+        }
+
+        RaycastHit noObjectHit;
+        if (Physics.Raycast(root.transform.position, root.transform.forward, out noObjectHit, Mathf.Infinity, NoObjectLayerMask))
+        {
+            var positionOffset = root.transform.forward * otherPlayer.localScale.x;
+
+            otherPlayer.transform.position = noObjectHit.point - positionOffset;
+
+            float distance = noObjectHit.distance;
+            float scaleMultiplier = distance / initialDistance;
+
+            otherPlayer.localScale = scaleMultiplier * initialSize * Vector3.one;
+        }
+    }
+
+    void OnDisable()
+    {
+        otherPlayer = null;
+    }
+
+    public void EnableMe(RaycastHit hit)
+    {
+        otherPlayer = hit.transform;
+        root = hit.transform;
+        initialSize = otherPlayer.transform.localScale.x;
+        initialDistance = hit.distance;
     }
 }
