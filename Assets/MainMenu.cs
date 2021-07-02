1af1397e85356a54540738a2fd81d13bd2816ada
Joshua Knaven <joshua.knaven@gmail.com>
2021-07-02 21:20:46 +0200

    MainMenu kamer setup

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string[] sceneNames;
+    public Color selectedColor;
+    public Color normalColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
